d1393f89bb6ac4192c315fd6e555314cbb2850d6
mirac <miracerdin@gmail.com>
2022-07-30 14:01:13 +0300

    arrranged

--- app.js
+++ app.js
@@ -3,29 +3,26 @@ const button = document.getElementById("button");
 const list = document.getElementsByClassName("list")[0];
 const removeBtn = `<button> REMOVE</button>`;
 let index = 0;
-
-const startConf = () => {
+window.addEventListener("load", () => {
   //baslangıç
   const todos = localStorage.getItem("todos");
-  // if (!todos) {
-  //   localStorage.setItem("todos", JSON.stringify([]));
-  // console.log(todos);
-  // } else {
-  let datalist = JSON.parse(localStorage.getItem("todos")) || []; // dataliste eğer json parse içi doluysa yani true ise aktar ama yoksa [] yi aktar demek || oluyor.
+  if (!todos) {
+    localStorage.setItem("todos", JSON.stringify([]));
+    console.log(todos);
+  } else {
+    let datalist = JSON.parse(localStorage.getItem("todos")); // dataliste eğer json parse içi doluysa yani true ise aktar ama yoksa [] yi aktar demek || oluyor.
 
-  datalist.forEach((e) => {
-    //? her bir todo objesini destructor yaptık
-    const { id, text, date } = e;
-    let element = document.createElement("div");
-    element.classList.add("altdiv");
-    element.innerHTML = `<input class="box" type = "checkbox"   />
+    datalist.forEach((e) => {
+      //? her bir todo objesini destructor yaptık
+      const { id, text, date } = e;
+      let element = document.createElement("div");
+      element.classList.add("altdiv");
+      element.innerHTML = `<input class="box" type = "checkbox"   />
     <p>${text}</p><div class="date">${date}<button class = "rmvBtn"> REMOVE</button></div>`;
-    list.appendChild(element);
-  });
-  // }
-};
-
-startConf();
+      list.appendChild(element);
+    });
+  }
+});
 
 button.addEventListener("click", addtToList);
 
@@ -74,28 +71,28 @@ function addtToList() {
     element.innerHTML = `<input class="box" type = "checkbox"   />
     <p>${input.value}</p><div class="date">${shortDate}<button class = "rmvBtn"> REMOVE</button></div>`;
     // localStorage.setItem(index, input.value);
-    let id1 = Math.floor(Math.random() * 10);
+    // let id1 = Math.floor(Math.random() * 10);
     let datalist = JSON.parse(localStorage.getItem("todos"));
-    miro();
+    index++;
+    // miro();
 
-    function miro() {
-      if (datalist.length > 0) {
-        let arr = datalist.filter((x) => x.id == id1);
-        if (arr.length != 0) {
-          id1 = Math.floor(Math.random() * 10);
-          return miro();
-        } else {
-          return id1;
-        }
-      }
-      return id1;
-    }
+    // function miro() {
+    //   if (datalist.length > 0) {
+    //     let arr = datalist.filter((x) => x.id == id1);
+    //     if (arr.length != 0) {
+    //       id1 = Math.floor(Math.random() * 10);
+    //       return miro();
+    //     } else {
+    //       return id1;
+    //     }
+    //   }
+    //   return id1;
+    // }
     let template = {
-      id: id1,
+      id: index,
       text: input.value,
       date: shortDate,
     };
-
     datalist.push(template);
     console.log(datalist);
 
@@ -110,17 +107,8 @@ function addtToList() {
   }
 }
 
-const box = document.querySelectorAll(".box");
+// const box = document.querySelectorAll(".box");
 // console.log(box);
 
 // const checkBoxes = document.querySelectorAll("input[type='checkbox']:checked");
 // console.log(checkBoxes);
-
-function clarusway() {
-  if (true) {
-    var a = 5;
-  }
-  document.write(a);
-}
-
-console.log(clarusway());
